e6b62d5c5dd6f7e308da6dc6d95d5ae8cacf1c65
Akanle Jeremy <akanlejeremy@gmail.com>
2024-10-01 10:07:34 +0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+  document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault()
+    
+    let li = document.createElement('li')
+    let description = document.getElementById("new-task-description").value
+    let color = document.getElementById("priority").value
+    // console.log(description)
+    li.textContent = description
+    li.style.color = color;
+
+    let del = document.createElement('button');
+    del.textContent = "Delete";
+    del.addEventListener('click', (e) => {
+      li.remove();
+      e.target.remove()
+    })
+    
+
+    document.querySelector('#tasks').appendChild(li);
+    document.querySelector('#tasks').appendChild(del);
+    let lists = document.querySelectorAll('#tasks li');
+    let tasksArray = Array.from(lists);
+
+    tasksArray.sort((a, b) => {
+      let order = {
+        "red": 1,
+        "yellow": 2,
+        "green": 3
+      }
+      return order[a.style.color] - order[b.style.color];
+    });
+
+    document.querySelector("#tasks").innerHTML = '';
+
+    tasksArray.forEach((task) => {
+      document.querySelector("#tasks").appendChild(task)
+    });
+    }
+
+)});
